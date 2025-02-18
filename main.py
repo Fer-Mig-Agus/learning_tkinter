95ec72e063ffd46a992e7917e4f4ab702b4baf61
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-02-18 16:26:15 -0300

    Parte del segundo video: falta terminar de verlo

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ window=tk.Tk()
 
 # Para colocar le titulo a la ventana
 window.title('Hola chivo')
+# Especificamos el tamaño de la ventana
+window.geometry('900x500')
+
+#Especificamos un tamaño minimo para la ventana
+window.minsize(400,300)
+#Especificamos un tamaño maximo para la ventana
+window.maxsize(1000,600)
 
 # Mantiene la ventana abierta
 window.mainloop()
