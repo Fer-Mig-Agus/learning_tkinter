655e52520b054898084481da20466303bcb0925c
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2025-02-18 10:39:58 -0300

    video 1: creacion de ventanas basicas

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+#Creo una instancia de la clase para crear la ventana
+window=tk.Tk()
+
+# Para colocar le titulo a la ventana
+window.title('Hola chivo')
+
+# Mantiene la ventana abierta
+window.mainloop()
+
